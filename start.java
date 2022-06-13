910f785744295fbe0829a24de6199bf4dbf09563
Orlova Diana <98831880+OrlovaDi@users.noreply.github.com>
2022-06-14 00:08:06 +0300

    Исправление формата

--- start.java
+++ start.java
@@ -1,9 +1,9 @@
-public class Main{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
         int i = 0;
         int sum = 0;
 
-        for (int a = 0; a < 5; a++){
+        for (int a = 0; a < 5; a++) {
             sum = sum + (i + 1);
             i++;
         }
